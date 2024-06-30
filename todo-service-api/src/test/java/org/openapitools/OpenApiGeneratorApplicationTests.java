eeb7b1a6425de271663ea1d52133e025cc70f47b
PaThAkavi <pathakavaneesh@gmail.com>
2024-06-30 23:51:20 +0530

    Fix todo service api test class

--- todo-service-api/src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
+++ todo-service-api/src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
@@ -1,9 +1,12 @@
 package org.openapitools;
 
-//@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class OpenApiGeneratorApplicationTests {
 
-//    @Test
+    @Test
     void contextLoads() {
     }
 
