f8341705d90a3ee0a9f578dabb6c75050b25a319
PaThAkavi <pathakavaneesh@gmail.com>
2024-06-30 17:55:33 +0530

    Fix outstanding issues with build

--- buildSrc/src/main/java/customtasks/InterpretYamlsTask.java
+++ buildSrc/src/main/java/customtasks/InterpretYamlsTask.java
@@ -5,12 +5,25 @@ import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.SkipWhenEmpty;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.internal.impldep.org.apache.commons.lang.StringUtils;
+//import org.openapitools.codegen.DefaultGenerator;
+//import org.openapitools.codegen.config.CodegenConfigurator;
 import util.TaskUtils;
 
-//import jakarta.inject.Inject;
+import jakarta.inject.Inject;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import static org.apache.commons.io.FileUtils.deleteQuietly;
+import static org.apache.commons.io.FileUtils.iterateFiles;
+import static org.gradle.internal.FileUtils.removeExtension;
 import static util.FileConstants.*;
 
 public class InterpretYamlsTask extends DefaultTask {
@@ -18,7 +31,7 @@ public class InterpretYamlsTask extends DefaultTask {
     private static final String DOTTED_PROJECT_PACKAGE = "models.yamls";
     private static String YAML_INPUT_FOLDER = RESOURCES_FOLDER + ("apis/yamls/").replace("/", FS);
 
-//    @Inject
+    @Inject
     public InterpretYamlsTask(TaskUtils t) {
         this.t = new TaskUtils(this);
     }
@@ -34,16 +47,27 @@ public class InterpretYamlsTask extends DefaultTask {
     public void runTask() {
         for (File file : getInputs().getFiles()) {
             String swaggerSourceFile = YAML_INPUT_FOLDER + file.getName();
-//            String extensionless = removeExtension(file.getName()).replace("-", "_");
-//            String extendedProjectPackage = DOTTED_PROJECT_PACKAGE + "._" + extensionless;
-//            generatedModelClasss(swaggerSourceFile, extendedProjectPackage);
+            String extensionless = removeExtension(file.getName()).replace("-", "_");
+            String extendedProjectPackage = DOTTED_PROJECT_PACKAGE + "._" + extensionless;
+            generatedModelClasses(swaggerSourceFile, extendedProjectPackage);
             cleanupUnusedOpenApiFiles();
-//            fixModels(extendedProjectPackage);
+            fixModels(extendedProjectPackage);
         }
     }
 
     private void generatedModelClasses(String swaggerSourceFile, String extendedProjectPackage) {
-
+//        CodegenConfigurator config = new CodegenConfigurator();
+//        config.setInputSpec(t.getFullPath(swaggerSourceFile).toString());
+//        config.setOutputDir(t.getProjDir());
+//        config.setGeneratorName("spring");
+//        config.addAdditionalProperty("modelPackage", extendedProjectPackage);
+//        config.addAdditionalProperty("sourceFolder", GENERATED_SRC_JAVA_FOLDER);
+//        config.addAdditionalProperty("serializableModel", true);
+//        config.addAdditionalProperty("openApiNullable", false);
+//        config.addAdditionalProperty("booleanGetterPrefix", "is");
+//        config.addAdditionalProperty("dateLibrary", "java17");
+//
+//        new DefaultGenerator().opts(config.toClientOptInput()).generate();
     }
 
     private void cleanupUnusedOpenApiFiles() {
@@ -56,4 +80,88 @@ public class InterpretYamlsTask extends DefaultTask {
         );
     }
 
+    private void fixModels(String extendedProjectPackage) {
+        File modelDirectory = new File(t.getProjDir() + GENERATED_SRC_JAVA_FOLDER + extendedProjectPackage.replace(".", FS));
+        Iterator<File> files = iterateFiles(modelDirectory, null, false);
+        while (files.hasNext()) {
+            File modelFile = files.next();
+            if (isAdsFrameworkClass(modelFile)) {
+                deleteQuietly(modelFile);
+            } else {
+                editFileContent(modelFile, extendedProjectPackage);
+            }
+        }
+    }
+
+    private boolean isAdsFrameworkClass(File modelFile) {
+        String[] adsFrameworkClasses = {
+                "GetConnectionStatusRequests",
+                "GetConnectionStatusResponse"
+        };
+        String extensionless = removeExtension(modelFile.getName());
+        for (String frameworkClass : adsFrameworkClasses) {
+            if (frameworkClass.equals(extensionless)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void editFileContent(File modelFile, String extendedProjectPackage) {
+        try {
+            Path path = modelFile.toPath();
+            String content = new String(Files.readAllBytes(path), charset);
+
+            content = addCamelCaseFormating(content);
+            content = editAccessors(content);
+
+            content = content.replaceAll("\\n.*JsonTypeInfo.As.WRAPPER_OBJECT[^\\n]*", "");
+
+            String frameworkModelPackage = "com.projects.todo.framework.model";
+            content = content.replace(extendedProjectPackage + ".SystemErrorDiagnostics", frameworkModelPackage + ".SystemErrorDiagnostics");
+            content = content.replace(extendedProjectPackage + ".SystemErrorResponse", frameworkModelPackage + ".SystemErrorResponse");
+            content = content.replace(extendedProjectPackage + ".StatusMessage", frameworkModelPackage + ".StatusMessage");
+            content = content.replace(extendedProjectPackage + ".SystemError", frameworkModelPackage + ".SystemError");
+
+            Files.write(path, content.getBytes(charset));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String addCamelCaseFormating(String content) {
+        Matcher m = Pattern.compile("(\\n.*)TO_CAMEL_CASE<(\\w)(\\w+)>([^\\n]*)").matcher(content);
+        String ignoredSubClasses = "String,Boolean,Byte,Character,Float,Double,Long,Short";
+        while (m.find()) {
+            String replacement;
+            if (ignoredSubClasses.contains(m.group(2) + m.group(3))) {
+                replacement = "";
+            } else {
+                String lowerFirst = m.group(2).toLowerCase();
+                replacement = m.group(1) + lowerFirst + m.group(3) + m.group(4);
+            }
+            content = content.replace(m.group(), replacement);
+        }
+        return content;
+    }
+
+    private String editAccessors(String content) {
+        ArrayList<String> fields = getFields(content);
+
+        for (String field: fields) {
+            String capitalizedFields = StringUtils.capitalise(field);
+            content = content.replace("is" + field, "is" + capitalizedFields);
+            content = content.replace("et" + field, "et" + capitalizedFields);
+        }
+        return content;
+    }
+
+    private ArrayList<String> getFields(String content) {
+        ArrayList<String> fields = new ArrayList<>();
+        Matcher fieldMatcher = Pattern.compile("private \\w+ (\\w+);").matcher(content);
+        while (fieldMatcher.find()) {
+            fields.add(fieldMatcher.group(1));
+        }
+        return fields;
+    }
 }
--- buildSrc/src/main/java/util/TaskUtils.java
+++ buildSrc/src/main/java/util/TaskUtils.java
@@ -1,9 +1,17 @@
 package util;
 
+import org.apache.commons.io.FileUtils;
 import org.gradle.api.Task;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static util.FileConstants.FS;
 
@@ -12,14 +20,23 @@ public class TaskUtils {
     public TaskUtils(Task t) {this.t = t;}
     public String getProjDir() { return t.getProject().getProjectDir().getPath() + FS;}
 
-    public Path geFullPath(String inProjectPath) {
+    public Path getFullPath(String inProjectPath) {
         String fullPath = getProjDir() + FS + inProjectPath;
         return Paths.get(fullPath);
     }
 
     public void deleteQuietly(String... inProjectFileLocations) {
         for (String loc : inProjectFileLocations) {
-//            FileUtils
+            FileUtils.deleteQuietly(new File(getProjDir() + FS + loc));
+        }
+    }
+
+    public void walkFiles(File rootDirectory, Consumer<Path> functionThatTakesPath) {
+        try (Stream<Path> walk = Files.walk(rootDirectory.toPath())) {
+            List<Path> result = walk.filter(Files::isRegularFile).collect(Collectors.toList());
+            result.forEach(functionThatTakesPath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
